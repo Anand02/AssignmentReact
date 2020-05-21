97f5840602b1a347db45af26e447e27fc96cf783
Anand <anand.sg@fleximc.com>
2020-05-21 23:48:01 +0530

    Updates

---
+++ precache-manifest.cf8426afbe8586fba6baffd7aed0729e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f42b28410350a143a61ad35bd13ab6f",
+    "url": "/AssignmentReact/index.html"
+  },
+  {
+    "revision": "749c861f5cbfd35b87bc",
+    "url": "/AssignmentReact/static/css/2.ba8a852c.chunk.css"
+  },
+  {
+    "revision": "d08d871730c3c01848c1",
+    "url": "/AssignmentReact/static/css/main.8a83bdcb.chunk.css"
+  },
+  {
+    "revision": "749c861f5cbfd35b87bc",
+    "url": "/AssignmentReact/static/js/2.c8faf359.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/AssignmentReact/static/js/2.c8faf359.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d08d871730c3c01848c1",
+    "url": "/AssignmentReact/static/js/main.dc93ab44.chunk.js"
+  },
+  {
+    "revision": "45e2e342eae21e77b1fc",
+    "url": "/AssignmentReact/static/js/runtime-main.9e3138b1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AssignmentReact/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/AssignmentReact/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/AssignmentReact/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/AssignmentReact/static/media/revicons.2feb69cc.eot"
+  }
+]);
